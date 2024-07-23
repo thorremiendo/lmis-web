e6bc93ab1ed7aab2269b1e3739a2100074fd626f
Thor Remiendo <thorremiendo@gmail.com>
2024-07-23 08:08:12 +0800

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-13-212-252-14.ap-southeast-1.compute.amazonaws.com/api',
+  apiUrl: 'https://calami-t-io.uc-bcf.edu.ph/api',
   mapboxToken: 'pk.eyJ1IjoiZGV2LXRob3JyZW1pZW5kbyIsImEiOiJjbHh0bWx5cHAxdDFxMmlzaDl4YWViczk1In0.P0CFPqf7yCWyolgD8m4GxA'
 };
